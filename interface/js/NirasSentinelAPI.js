cf020f4defdf1d20b2b90316f87f438b067d6bef
Casper Samsø Fibæk (CFI) <CFI@niras.int>
2017-02-17 15:53:13 +0100

    reading transition to nodeJS

--- interface/js/NirasSentinelAPI.js
+++ interface/js/NirasSentinelAPI.js
@@ -1,9 +1,10 @@
-var NirasSentinelAPI = (function () {
-  // Internal Constructor
-  var api = function () {
-    this.version = '0.1.1'
+/* globals SunCalc */
 
-    // regular calculation here.
+const NirasSentinelAPI = (function () {
+  // Internal Constructor
+  const api = function () {
+    this.version = '0.1.2'
+
     this.getParamObj = function (arr, geom) {
       let returnObj = {
         'clouds': {},
@@ -41,10 +42,53 @@ var NirasSentinelAPI = (function () {
 
     this.getESAString = function (obj) {
       const baseUrl = 'https://scihub.copernicus.eu/dhus/search?q='
-      return `${baseUrl}footprint:"Intersects(${obj.footprint})" AND platformname:${obj.satellite} AND cloudcoverpercentage:[${obj.clouds.from} TO ${obj.clouds.to}] AND beginposition:[${obj.timeline.from} TO ${obj.timeline.to}]&format=json`
+      return `${baseUrl}footprint:"Intersects(${obj.footprint})" AND platformname:${obj.satellite} AND cloudcoverpercentage:[${obj.clouds.from} TO ${obj.clouds.to}] AND beginposition:[${obj.timeline.from} TO ${obj.timeline.to}]&start=0&rows=100&format=json`
+    }
+
+    this.makeSense = function (obj) {
+      var m = obj.feed
+      let metaData = {
+        'amount': m.entry.length,
+        'searchParam': m.id,
+        'images': []
+      }
+
+      for (let i = 0; i < metaData.amount; i++) {
+        var curr = m.entry[i]
+
+        let image = {
+          'date': {},
+          'clouds': Number(curr.double.content),
+          'footprint': {},
+          'information': {},
+          'sunAngle': {},
+          'thumbnail': curr.link[2],
+          'link': curr.link[0],
+          'alternativeLink': curr.link[1]
+        }
+
+        for (let w = 0; w < curr.date.length; w++) {
+          image.date[curr.date[w].name] = curr.date[w].content
+        }
+
+        for (let j = 0; j < curr.str.length; j++) {
+          image.information[curr.str[j].name] = curr.str[j].content
+        }
+
+        image.footprint = image.information.footprint
+        image.sunAngle = SunCalc.getPosition(
+          new Date(image.date.beginposition), 55.40, 10.40
+        )
+        image.sunAngle.azimuth = (image.sunAngle.azimuth * 180) / Math.PI
+        image.sunAngle.altitude = (image.sunAngle.altitude * 180) / Math.PI
+
+        metaData.images.push(image)
+      }
+
+      return metaData
     }
   }
   return api
 })()
 
-var nrs = new NirasSentinelAPI() // eslint-disable-line
+const nrs = new NirasSentinelAPI() // eslint-disable-line
---
+++ interface/js/init.js
@@ -0,0 +1,96 @@
+/* globals $ nrs L Wkt */
+let map = L.map('map', {
+  center: [ 55.3322691334024, 10.3491210937499 ],
+  zoom: 6,
+  maxZoom: 12,
+  minZoom: 5,
+  editable: true,
+  zoomControl: false,
+  attributionControl: false
+})
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 21,
+  maxNativeZoom: 18
+}).addTo(map)
+
+/*
+ * Default Geometry
+ */
+
+// Fionia, Denmark
+const defGeomLatLng = [
+  [54.575245800783310, 9.52514648437500],
+  [54.575245800783310, 11.1126708984375],
+  [55.936894769039434, 11.1126708984375],
+  [55.936894769039434, 9.52514648437500],
+  [54.575245800783310, 9.52514648437500]
+]
+let defGeomLayer = L.polygon(defGeomLatLng, {
+  color: '#ff0033',
+  draggable: true
+}).addTo(map)
+defGeomLayer.enableEdit()
+defGeomLayer
+  .on('dblclick', L.DomEvent.stop)
+  .on('dblclick', defGeomLayer.toggleEdit)
+
+/*
+ * EVENT LISTENERS
+ */
+$('.getSat').click(function () {
+  if (validateForm() === true) {
+    let formData = $('.paramForm').serializeArray()
+    let wkt = new Wkt.Wkt()
+    wkt.read(JSON.stringify(defGeomLayer.toGeoJSON()))
+
+    console.log('link: ', nrs.getESAString(nrs.getParamObj(
+       formData,
+       wkt.write()
+     )))
+  }
+})
+
+$('.formBox.satellite > p').click(function () {
+  $(this).prev().click()
+})
+
+$('.reset').click(function () {
+  map.panTo([55.33226, 10.34912])
+  defGeomLayer
+    .setLatLngs(defGeomLatLng)
+    .redraw()
+    .toggleEdit()
+  defGeomLayer.toggleEdit()
+})
+
+/*
+ * Basic functions
+ */
+const validateForm = function () {
+  let cloudsFrom = Number($("input[name='clouds-from']").val())
+  let cloudsTo = Number($("input[name='clouds-to']").val())
+  if (cloudsFrom >= cloudsTo) {
+    alert('Clouds cover from >= to')
+    return false
+  }
+
+  let dateFrom = new Date($("input[name='date-from']").val()).getTime()
+  let dateTo = new Date($("input[name='date-to']").val()).getTime()
+  let dateToday = new Date().getTime()
+  let dateSentinel = new Date('2015-06-23').getTime()
+  if (dateFrom >= dateTo) {
+    alert('Start date before end date')
+    return false
+  } else if (dateFrom > dateToday || dateTo > dateToday) {
+    alert("You've selected a date in the future.")
+    return false
+  } else if (dateFrom < dateSentinel || dateTo < dateSentinel) {
+    alert('Sentinel-2A was launced the 23rd of June 2015')
+    return false
+  }
+
+  return true
+}
